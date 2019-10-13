a08381819145bf34010e28e2938689845bcff9fe
Emerson Antonio da Silva <emerson@afirmanet.com.br>
2019-10-13 14:16:36 -0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -4,35 +4,43 @@ var sass  =  require('gulp-sass');
 
 
 function sassTask (cb) {
-    src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    src(['node_modules/bootstrap/scss/bootstrap.scss','node_modules/font-awesome/scss/font-awesome.scss', 'public/scss/*.scss'])
     .pipe(sass())
-    .pipe(dest("src/css"))
+    .pipe(dest("./public/css"))
     .pipe(browserSync.stream());
     
     cb();
 };
 
 function jsTask (cb) {
-    src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 
-                     'node_modules/jquery/dist/jquery.min.js', 
-                     'node_modules/popper.js/dist/umd/popper.min.js'])
-    .pipe(dest("src/js"))
+    src(['node_modules/bootstrap/dist/js/bootstrap.js', 
+                     'node_modules/jquery/dist/jquery.js', 
+                     'node_modules/popper.js/dist/umd/popper.js'])
+    .pipe(dest("./public/js"))
     .pipe(browserSync.stream());
     
+    cb();
+};
+
+function fontsTask (cb) {
+    src(['node_modules/font-awesome/fonts/*.*'])
+    .pipe(dest("./public/fonts"))
+    .pipe(browserSync.stream());
+
     cb();
 };
 
 function defaultTask(cb) {
     browserSync.init({
         server: {
-            baseDir: "./src"
+            baseDir: "./public"
         }
     });
 
-    watch(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'], sassTask);
-    watch("*.html").on('change', browserSync.reload);
+    watch(['node_modules/bootstrap/scss/*.scss', 'node_modules/font-awesome/scss/*.scss', 'public/scss/*.scss'], sassTask);
+    watch("./public/*.html").on('change', browserSync.reload);
 
     cb();
 }
 
-exports.default = series(parallel(jsTask, sassTask), defaultTask);
+exports.default = series(parallel(fontsTask, jsTask, sassTask), defaultTask);
